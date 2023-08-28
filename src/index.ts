9efe400facb595ebb2643166ee4168ab235ba03b
daniel <danielbyomujuni@gmail.com>
2023-08-28 00:52:51 +0000

    Create the Basic Project layout

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+function main() {
+  console.log("Test")
+
+  //DROP EXISTING DB IF EXISTS
+
+  //PARSE JSON
+
+  //Get All the Tables From JSON
+  
+    //Get Column with TYPE
+    
+    //Get Primary Key
+
+    //Get Contstraints
+
+    //Build The SQL COMMNAD
+
+}
+
+main()
