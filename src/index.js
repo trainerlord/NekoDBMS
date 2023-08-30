87a3f26d0464c8202b4380f59abc054518df8c09
daniel <danielbyomujuni@gmail.com>
2023-08-30 01:04:27 +0000

    created sql Instructions

--- src/index.js
+++ src/index.js
@@ -7,9 +7,34 @@ function main() {
     var json = getJson("./src/database.json");
     var schema = json.schema;
     var tables = getJsonKeys(schema);
-    console.log(tables);
-    console.log(schema[0][tables[0]]);
+    var sqlCommands = [];
+    //console.log(schema[0][tables[0]]['Columns'])
     //DROP EXISTING DB IF EXISTS
+    sqlCommands.push("CREATE DATABASE testing;");
+    for (var i = 0; i < tables.length; i++) {
+        var createTable = "CREATE TABLE ".concat(tables[i], "(");
+        var cols = schema[i][tables[i]]['Columns'];
+        //console.log(cols)
+        for (var c in cols) {
+            //console.log(c)
+            createTable += "".concat(c, " ").concat(schema[i][tables[i]]['Columns'][c], ",");
+        }
+        createTable += "PRIMARY KEY (".concat(schema[i][tables[i]]['Primary_Key'], ");");
+        sqlCommands.push(createTable);
+        if (schema[i][tables[i]]["Constraints"] == undefined) {
+            continue;
+        }
+        var index = 0;
+        for (var fk in schema[i][tables[i]]["Constraints"]) {
+            var constrain = schema[i][tables[i]]["Constraints"];
+            console.log(constrain[fk]);
+            var foreignConstrants = "ALTER TABLE ".concat(tables[i], " ADD CONSTRAINT fk-").concat([tables[i]], "-").concat(index, " FOREIGN KEY (").concat(constrain[fk]['Child'].split('.')[0], ") REFERENCES ").concat(constrain[fk]['Parent'].replace('.', ' ('), ");");
+            sqlCommands.push(foreignConstrants);
+            index += 1;
+        }
+        //Constraints
+    }
+    console.log(sqlCommands);
     //PARSE JSON
     //Get All the Tables From JSON
     //Get Column with TYPE
--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,50 @@ function main() {
 
   let tables = getJsonKeys(schema)
 
-  console.log(tables)
-  console.log(schema[0][tables[0]])
+  let sqlCommands: string[] = []
+
+  //console.log(schema[0][tables[0]]['Columns'])
   //DROP EXISTING DB IF EXISTS
+  sqlCommands.push("CREATE DATABASE testing;")
+
+  for(let i = 0; i < tables.length; i++) {
+    let createTable = `CREATE TABLE ${tables[i]}(`
+    const cols = schema[i][tables[i]]['Columns']
+
+    //console.log(cols)
+    for(let c in cols) {
+      //console.log(c)
+
+      createTable += `${c} ${schema[i][tables[i]]['Columns'][c]},`
+    }
+
+    createTable += `PRIMARY KEY (${schema[i][tables[i]]['Primary_Key']});`
+    
+
+
+    sqlCommands.push(createTable)
+    
+    if (schema[i][tables[i]]["Constraints"] == undefined) {
+      continue;
+    }
+
+    let index = 0
+    for (let fk in schema[i][tables[i]]["Constraints"]) {
+      const constrain = schema[i][tables[i]]["Constraints"]
+        console.log(constrain[fk])
+
+      let foreignConstrants: string = `ALTER TABLE ${tables[i]} ADD CONSTRAINT fk-${[tables[i]]}-${index} FOREIGN KEY (${constrain[fk]['Child'].split('.')[0]}) REFERENCES ${constrain[fk]['Parent'].replace('.', ' (')});`
+        sqlCommands.push(foreignConstrants);
+
+      index += 1
+
+      
+    }
+    //Constraints
+  }
+
+
+  console.log(sqlCommands)
 
   //PARSE JSON
 
