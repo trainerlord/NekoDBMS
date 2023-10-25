8b0c79af90b9ff644f2161c494c4cab0592be18f
Daniel <danielbyomujuni@gmail.com>
2023-10-25 20:32:03 +0000

    Create C++ Project Structure

--- src/steps/parseJson.ts
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-export default function parseJson(db_name:string, path: String): any {
-    const jsonString = fs.readFileSync(path, "utf-8");
-    const object = JSON.parse(jsonString);
-    return object;
-}
